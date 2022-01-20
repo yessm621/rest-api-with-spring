e3c62b733ad0e5bdf71884a2fbb2dd43f262ccda
yessm621 <yessm621@gmail.com>
2022-01-20 17:04:05 +0900

    Body for Bad Request

--- src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
@@ -121,6 +121,10 @@ public class EventControllerTests {
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists());
     }
 }
